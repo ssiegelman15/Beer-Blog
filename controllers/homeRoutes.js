f9f7940e3603a1a6c9cf46d4246023e64d032677
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-21 18:33:12 -0600

    added auth.js to helpers, created homeRoutes file with bare bones

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+// const { Project, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

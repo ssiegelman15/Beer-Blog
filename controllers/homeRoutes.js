39c2c15bb4b11e6ad7695c9a38ce98395afc31fe
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-23 20:22:16 -0600

    corrected all profile references to dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -47,7 +47,7 @@ router.get("/post/:id", async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get("/profile", withAuth, async (req, res) => {
+router.get("/dashboard", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -56,8 +56,8 @@ router.get("/profile", withAuth, async (req, res) => {
     });
     // Serialize user data
     const user = userData.get({ plain: true });
-    // Pass serialized data and session into profile template
-    res.render("profile", {
+    // Pass serialized data and session into dashboard template
+    res.render("dashboard", {
       ...user,
       logged_in: true,
     });
@@ -70,7 +70,7 @@ router.get("/profile", withAuth, async (req, res) => {
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect("/profile");
+    res.redirect("/dashboard");
     return;
   }
   // Render login display

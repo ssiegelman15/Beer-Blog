726d0dad1f273b4e8603baa7b8b9c61d95394f77
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-23 19:10:39 -0600

    created get route to fetch post by id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,6 +4,7 @@ const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
+    // Collect all posts and join with User data
     const postData = await Post.findAll({
       include: [
         {
@@ -11,11 +12,32 @@ router.get("/", async (req, res) => {
         },
       ],
     });
-
+    // Serialize post data
     const posts = postData.map((post) => this.post.get({ plain: true }));
-
+    // Pass serialized data and session into homepage template
     res.render("homepage", {
       posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render("post", {
+      ...post,
       logged_in: req.session.logged_in,
     });
   } catch (err) {

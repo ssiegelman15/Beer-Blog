2d163a2d6cc24f49c69972761c14bdbc80206b6b
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-23 20:52:35 -0600

    created seed.js file to seed database

---
+++ seeds/seed.js
@@ -0,0 +1,27 @@
+const sequelize = require("../config/connection");
+const { User, Comment, Post } = require("../models");
+
+const userData = require("./userData.json");
+const postData = require("./postData.json");
+const commentData = require("./commentData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const posts = await Post.bulkCreate(postData, {
+    returning: true,
+  });
+
+  const comments = await Comment.bulkCreate(commentData, {
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();

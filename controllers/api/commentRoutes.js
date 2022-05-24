ca3043a50401935dea8c512db6113fe60bd4cdc4
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-23 20:14:56 -0600

    created post route for new comments on existing posts

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -11,3 +11,17 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// Create new comment on post
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.create({
+      userId: req.session.userId,
+      postId: req.body.postId,
+      comment: req.body.comment,
+    });
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});

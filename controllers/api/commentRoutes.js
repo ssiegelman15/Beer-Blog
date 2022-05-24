063fc8d78449cbeacd8c85d92ca1db921228cffc
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-23 20:19:20 -0600

    added delete route to delete comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -25,3 +25,25 @@ router.post("/", withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// Delete comment
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!commentData) {
+      res
+        .status(404)
+        .json({ message: "No comment was found that matches that ID." });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

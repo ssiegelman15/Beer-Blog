d57b1ff1c61b6b1ceb1d5aff5bee9823409f85c2
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-23 18:52:39 -0600

    created homepage get route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,6 +4,20 @@ const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => this.post.get({ plain: true }));
+
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }

70302fdac7d5298ac7bd5c89d8727ad7d7ec3f03
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-25 21:19:35 -0600

    finished writing comments.js file

---
+++ public/js/comments.js
@@ -0,0 +1,28 @@
+const newFormHandler = async (event) => {
+  if (event.target.hasAttribute("dataId")) {
+    const postId = event.target.getAttribute("dataId");
+    const commentContent = document
+      .querySelector("#commentContent")
+      .value.trim();
+
+    if (commentContent && postId) {
+      const response = await fetch(`/api/comments`, {
+        method: "POST",
+        body: JSON.stringify({ commentContent, postId }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        document.location.replace(`/posts/${postId}`);
+      } else {
+        alert("Comment could not be created, please try again.");
+      }
+    }
+  }
+};
+
+document
+  .querySelector(".newCommentForm")
+  .addEventListener("submit", newFormHandler);
